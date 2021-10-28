8fb8877863366be92d11baf46fa799b9db420ca8
NotSimpleCode <onejobsdiego@gmail.com>
2021-10-27 23:21:23 -0500

    confirmo cambios

--- forms/formularios.py
+++ forms/formularios.py
@@ -1,6 +1,6 @@
 from flask.app import Flask
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField
 from wtforms.validators import DataRequired
 
 class Login(FlaskForm):
@@ -11,7 +11,18 @@ class Login(FlaskForm):
 class Registro(FlaskForm):
     username = StringField("Usuario")
     nombre = StringField("Nombre")
-    correo = StringField("Correo")
+    email = StringField("Correo")
     password = PasswordField("Password")
     registrar = SubmitField("Registrar")
+    id_rol = SelectField("Rol", coerce=int, choices=[("0","--Rol--"),("1","Usuario"),("2","Administrador"),("3","Superadministrador")], validators=[DataRequired(message="Campo en blanco")])
+
+class comentarios(FlaskForm):
+    id_comentario = StringField("id_comentario")
+    comentario = TextAreaField("Comentario")
+    titulo = StringField("Pelicula")
+    nombre = StringField("Usuario")
+    comentar = SubmitField("Enviar")
+
+
+    
     
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask,redirect,request,flash,session,escape,render_template
 import sqlite3
 import os
-from forms.formularios import Login, Registro
+from forms.formularios import Login, Registro,comentarios
 import hashlib
 
 app = Flask(__name__)
@@ -15,23 +15,34 @@ def home():
 def login():
     frm = Login()
     if frm.validate_on_submit():
-        username = frm.username.data
-        password = frm.password.data
+        username = escape(frm.username.data)
+        password = escape(frm.password.data)
         # Cifra la contraseña
         enc = hashlib.sha256(password.encode())
         pass_enc = enc.hexdigest()
 
         #Conectar a la BD
-        with sqlite3.connect("usuarios.db") as con:
+        with sqlite3.connect("cineRoyal.db") as con:
             # Crea cursos para manipular la BD
+            con.row_factory = sqlite3.Row
             cursor = con.cursor()
             #Prepara la sentencia SQL a ejecutar
-            cursor.execute("SELECT username FROM usuario WHERE username = ? AND password = ?", [username, pass_enc])
-            if cursor.fetchone():
-                return "Bienvenido!!!"
+            cursor.execute("SELECT username, id_usuario ,nombre, id_rol FROM usuario WHERE username = ? AND password = ?", [username, pass_enc])
+            row = cursor.fetchone()
+            if row:
+                session["usuario"] = username
+                session["ide"] = row["id_usuario"]
+                session["nombre"] = row["nombre"]
+                session["id_rol"] = row["id_rol"]
+                
+                if session["id_rol"] == 1:
+                    return redirect("/perfil")
+                elif session['id_rol'] == 2:
+                    return "No definido"
+                elif session["id_rol"] == 3:
+                    return "Tampoco"
             else:
-                return "Usuario/Password errado"
-            #Ejecuta la sentencia SQL
+                return f"Usuario/Password errados"
 
     return render_template("ingreso.html", frm = frm)
 
@@ -44,18 +55,19 @@ def registrar(): #Endpoint
         #Captura los datos del formulario
         username = frm.username.data
         nombre = frm.nombre.data
-        correo = frm.correo.data
+        email = frm.email.data
         password = frm.password.data
+        id_rol = frm.id_rol.data
         # Cifra la contraseña
         enc = hashlib.sha256(password.encode())
         pass_enc = enc.hexdigest()
 
         #Conectar a la BD
-        with sqlite3.connect("usuarios.db") as con:
+        with sqlite3.connect("cineRoyal.db") as con:
             # Crea cursos para manipular la BD
             cursor = con.cursor()
             #Prepara la sentencia SQL a ejecutar
-            cursor.execute("INSERT INTO usuario (nombre, username, correo, password) VALUES (?,?,?,?)", [nombre, username, correo, pass_enc])
+            cursor.execute("INSERT INTO usuario (nombre, email, username, password,id_rol) VALUES (?,?,?,?,?)", [nombre,email,username,pass_enc,id_rol])
             #Ejecuta la sentencia SQL
             con.commit()
             return "Guardado con éxito"
@@ -67,14 +79,76 @@ def buscar():
 
 @app.route("/perfil")
 def perfil():
-    return render_template("profile.html")
+    if "usuario" in session and session["id_rol"]==1:
+        return render_template("profile.html")
 
 @app.route("/cartelera")
 def cartelera():
     return render_template("cartelera.html")
+####################################################################################################################################
+######################################################################################################################
+##################################################################################################################
+
+
+#----------------------------------------CREAR CRUD COMENTARIOS ------------------------------------------------#
+@app.route('/comentarios', methods=["GET","POST"])
+
+def comentario():
+        frm = comentarios()#Instancia de la clase en formulario.py
+        if frm.validate_on_submit():
+          #Recupera datos
+            
+            comentario = frm.comentario.data
+            titulo = frm.titulo.data
+            nombre = frm.nombre.data
+            with sqlite3.connect("cineRoyal.db") as con:
+                # Crea cursos para manipular la BD
+                cursor = con.cursor()
+                #Prepara la sentencia SQL a ejecutar
+                cursor.execute ("INSERT INTO comentario (comentario,titulo,nombre) VALUES (?,?,?)",[comentario,titulo,nombre])
+                con.commit()
+                return "Guardado con éxito"
+        return render_template("comentarios.html", frm= frm) #Respuesta    
+
+#----------------------------------------EDITAR CRUD COMENTARIOS ------------------------------------------------#
+@app.route('/comentarios/actualizar/', methods=["POST","GET"])
+def actualizarC():
+        form = comentarios()#Instancia de la clase en formulario.py
+        if request.method == "POST":
+            nombre = form.nombre.data
+       
+            with sqlite3.connect("cineRoyal.db") as conn:
+                cur = conn.cursor()
+                cur.execute(
+                    "UPDATE comentario SET mensaje = ? WHERE nombre = ?", [nombre]
+                )
+                conn.commit()#Confirmación de inserción de datos :)
+                return "¡Datos actualizados exitosamente ^v^!"
+        return "No se pudo actualizar "
+#----------------------------------------VISUALIZAR CRUD COMENTARIO ---------------------------------------------#
+# @app.route('/comentarios/eliminar/', methods=["POST"])
+# def eliminarC():
+   
+#     form = comentarios()
+#     if request.method == "POST":
+#         docum = form.documento.data
+#         with sqlite3.connect("cineRoyal.db") as conn:
+#             conn.row_factory = sqlite3.Row
+#             cur = conn.cursor()#manipula la db
+#             cur.execute("DELETE FROM comentario WHERE id_comentario = ?", [docum])
+#             if conn.total_changes > 0:
+#                 return "Comentario  borrado ^v^"
+#             return render_template("comentarios.html")
+#     return "Error"
+#----------------------------------------BORRAR CRUD COMENTARIOS ------------------------------------------------#
 
 @app.route("/funciones")
 def funciones():
     return render_template("funciones.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
 
 app.run(debug=True)
