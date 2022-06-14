f4abd090c72d13759b0305c7783fae0bc350c41d
NotSimpleCode <onejobsdiego@gmail.com>
2022-06-13 21:27:46 -0500

    13/0/2022

--- forms/formularios.py
+++ forms/formularios.py
@@ -9,20 +9,6 @@ class Login(FlaskForm):
     enviar = SubmitField ("Iniciar sesion") 
     
 class Registro(FlaskForm):
-<<<<<<< HEAD
-    username = StringField("Usuario", validators=[DataRequired("Usuario es obligatorio")])
-    nombre = StringField("Nombre", validators=[DataRequired("Nombre es obligatorio")])
-    email = StringField("Correo", validators=[DataRequired("Correo es obligatorio")])
-    password = PasswordField("Password", validators=[DataRequired("Password es obligatorio")])
-    id_rol = SelectField("Rol", coerce=int, choices=[("0","--Escoja una opción--"),("1","Usuario"),("2","Administrador"),("3","Superadministrador")], validators=[DataRequired(message="Rol es obligatorio")])
-    registrar = SubmitField("Registrar")
-
-class Comentarios(FlaskForm):
-    id_comentario = SelectField("ID comentario", coerce=int, choices=[("0","--Edite las opciones--")], validators=[DataRequired("ID comentario es obligatorio")])
-    comentario = TextAreaField("Comentario", validators=[DataRequired("Comentario es obligatorio")])
-    titulo = StringField("Pelicula", validators=[DataRequired("Nombre de la pelicula es obligatorio")])
-    nombre = StringField("Nombre", validators=[DataRequired("Nombre es obligatorio")])
-=======
     username = StringField("Usuario")
     nombre = StringField("Nombre")
     email = StringField("Correo")
@@ -35,7 +21,6 @@ class comentarios(FlaskForm):
     comentario = TextAreaField("Comentario")
     titulo = StringField("Pelicula")
     nombre = StringField("Usuario")
->>>>>>> 05194d25a9477aabdbff3ad5372932961dd6e079
     comentar = SubmitField("Enviar")
 
 class funcion(FlaskForm):
